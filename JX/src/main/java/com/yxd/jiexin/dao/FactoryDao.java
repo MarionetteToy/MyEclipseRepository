c780868c66e459c5fd077bae70f23b880c24e057
MarionetteToy <1072988987@qq.com>
2017-08-30 11:56:30 +0800

    杰信项目

--- JX/src/main/java/com/yxd/jiexin/dao/FactoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.yxd.jiexin.dao;
-
-import java.util.Map;
-
-import com.yxd.jiexin.domain.Factory;
-
-public interface FactoryDao extends BaseDao<Factory> {
-	public void updateState(Map map);			//修改状态
-}
--- JX/src/main/webapp/index.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html; charset=utf-8"%>
-<html>
-<head>
-</head>
-<body>
-	<h1>首页</h1>
-	<a href="${pageContext.request.contextPath }/home.action">由此去</a>
-</body>
-</html>
